cf8f42a367369f75086514bc8819030dbdc5e9ff
Cecelia-1018 <53387160+Cecelia-1018@users.noreply.github.com>
2022-03-08 17:10:07 +0800

    upload
    yarn add react-image-magnifiers
    
    if u hv error

--- supermama-admin/src/components/verifyPro.js
+++ supermama-admin/src/components/verifyPro.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import {
   collection,
   query,
@@ -9,35 +9,52 @@ import {
   updateDoc,
   deleteDoc,
 } from "firebase/firestore";
-import { Button } from "react-bootstrap";
+import { Button, Overlay, Popover, Header } from "react-bootstrap";
 import { db } from "../firebase";
 import { useSelector } from "react-redux";
 import { useFirestoreConnect, isLoaded, isEmpty } from "react-redux-firebase";
 //Calling Bootstrap 4.5 css
 import "bootstrap/dist/css/bootstrap.min.css";
 import Table from "material-table";
+import {
+  Magnifier,
+  GlassMagnifier,
+  SideBySideMagnifier,
+  PictureInPictureMagnifier,
+  MOUSE_ACTIVATION,
+  TOUCH_ACTIVATION,
+} from "react-image-magnifiers";
 
-function VerifyPro(){
-    const [verifyPro, SetVerifyPro] = useState([]);
-    
-    useEffect(() => {
-       const q = query(collection(db, "verifyPro"));
-       onSnapshot(q, (querySnapshot) => {
-         SetVerifyPro(
-           querySnapshot.docs.map((doc) => ({
-             id: doc.id,
-             data: doc.data(),
-           }))
-         );
-       });
-     }, []);
+function VerifyPro() {
+  const [verifyPro, SetVerifyPro] = useState([]);
 
-     if(isEmpty(verifyPro)) {
-       return "No verification";
-     }
+  const [show, setShow] = useState(false);
+  const [target, setTarget] = useState(null);
+  const ref = useRef(null);
 
-    return (
-      <div>
+  const handleClick = (event) => {
+    setShow(!show);
+    setTarget(event.target);
+  };
+
+  useEffect(() => {
+    const q = query(collection(db, "verifyPro"));
+    onSnapshot(q, (querySnapshot) => {
+      SetVerifyPro(
+        querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      );
+    });
+  }, []);
+
+  if (isEmpty(verifyPro)) {
+    return "No verification";
+  }
+
+  return (
+    <div>
       <div className="container">
         <table id="example" class="display table">
           <thead class="thead-dark">
@@ -50,43 +67,42 @@ function VerifyPro(){
           </thead>
           <tbody>
             {verifyPro.map((verify) => (
-             <tr>
-              <td align="left">
+              <tr>
+                <td align="left">
                   <b>VerifyID:</b> <br />
                   {verify.id}
-                  <br /><b>User Id:</b> <br />
+                  <br />
+                  <b>User Id:</b> <br />
                   {verify.data.userId} <br />
                   <b>Professional Field:</b> <br />
                   {verify.data.proField} <br />
-                  
-              </td>
+                </td>
 
-             <td>Picture</td>
+                <td align="center">
+                <GlassMagnifier
+                            imageSrc={verify.data.photoURL}
+                            imageAlt="Certificate"
+                            style={{ width: 500, height: 600 ,}}
+                            largeImageSrc={verify.data.photoURL} // Optional
+                            
+                          /> 
+                </td>
 
-             <td>{verify.data.status}</td>
-             
-             <td>
-                  
-             <Button variant="outline-primary">Verify</Button>
+                <td>{verify.data.status}</td>
+
+                <td>
+                  <Button variant="outline-primary">Verify</Button>
                   <br />
                   <br />
                   <Button variant="danger">Reject</Button>
                 </td>
-
-             </tr>
-            
-            ))} 
-        
-     
-          
-    
+              </tr>
+            ))}
           </tbody>
-         </table>
+        </table>
       </div>
-     </div>
-       
-        
-    )
+    </div>
+  );
 }
 
 export default VerifyPro;
